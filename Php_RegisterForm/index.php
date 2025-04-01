0d572d60905dafc3a320cdb006213b6b1d844dac
Chaimaeelba <elbahchoumichaimae1@gmail.com>
2025-04-01 17:23:09 +0000

    Update index.php

--- Php_RegisterForm/index.php
+++ Php_RegisterForm/index.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Form</title>
+</head>
+<body>
+    <form action="cible.php" method="POST">
 
+        <label for="firstname">First Name:</label>
+        <input type="text" id="firstname" name="firstname" ><br><br>
+
+        <label for="lastname">Last Name:</label>
+        <input type="text" id="lastname" name="lastname" ><br><br>
+
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" ><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" ><br><br>
+
+        <label for="confirm_password">Confirm Password:</label>
+        <input type="password" id="confirm_password" name="confirm_password" ><br><br>
+
+        <label>Gender:</label>
+            <input type="radio" name="gender" value="male"> Male
+            <input type="radio" name="gender" value="female"> Female <br><br>
+
+        <button type="submit">Register</button>
+
+</body>
+</html>
